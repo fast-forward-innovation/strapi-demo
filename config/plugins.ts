8dc0526563e0d4da2e512c25ab75dbf67e52968f
Jason Yarrington <jason@fastforward.sh>
2025-07-03 14:15:50 -0600

    Enable apollo playground

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+        config: {
+        playgroundAlways: true,   // ✅ Force enable Playground
+        defaultLimit: 100,
+        maxLimit: 250,
+        apolloServer: {
+            introspection: true,    // ✅ Allow introspection in production
+        },
+    },
+  },
+});
