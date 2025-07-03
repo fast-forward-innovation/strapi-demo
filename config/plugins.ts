ee4b253b6a3aa74cf2d0aa1ae23db4c208ead2b3
Jason Yarrington <jason@fastforward.sh>
2025-07-03 14:19:33 -0600

    Enable apollo sandbox

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,12 @@
 export default () => ({
     graphql: {
         config: {
-        playgroundAlways: true,   // ✅ Force enable Playground
-        defaultLimit: 100,
-        maxLimit: 250,
-        apolloServer: {
-            introspection: true,    // ✅ Allow introspection in production
+            playgroundAlways: true,   // ✅ Force enable Playground
+            defaultLimit: 100,
+            maxLimit: 250,
+            apolloServer: {
+                introspection: true,    // ✅ Allow introspection in production
+            },
         },
     },
-  },
 });
